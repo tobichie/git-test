ce353e2b1f8cfecdd49d623c702a543eb24ea649
tobichie <tyron.obichie4@gmail.com>
2024-01-25 14:03:31 +0100

    #

--- php/Finanzen/Manager/functions.php
+++ php/Finanzen/Manager/functions.php
@@ -6,16 +6,31 @@ if (!$conn) {
 } else {
     function view($conn)
     {
+        echo "<table style='border: 1px solid black; position: relative; top: 50%; left: 50%; transform: translate(-50%, -50%);'>";
+        ?>
+            <tr>
+                <td>Title</td>
+                <td>Time</td>
+                <td>Amount</td>
+            </tr>
+        <?php
         // show the entries in the database
         $sql = "SELECT * FROM finanzen.finanz";
         $result = $conn->query($sql);
-        if ($result->num_rows()>0){
-            while ($rows = $result->fetch_assoc){
-                echo "<div>" . $rows["Name"] . "</div>";
+        if ($result->num_rows > 0) {
+            while ($rows = $result->fetch_assoc()) {
+                // inside of the table
+                echo "<tr getID='" . $rows["ID"] . "'>";
+                echo "<td>" . $rows["Title"] . "</td>";
+                echo "<td>" . $rows["Time"] . "</td>";
+                echo "<td>" . $rows["Amount"] . "</td>";
+                echo "</tr>";
             }
         }
+        echo "</table>";
 
     }
+
     view($conn);
 
 }
